97ca567c525a58539f0bd6bb18839bed3f6a56b9
wuyanbing <Mr_wuyuan@163.com>
2020-11-23 15:54:13 +0800

    '切割头部和尾部子视图'

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,33 @@
+<style>
+    footer {
+        margin-top: 45px;
+        padding-top: 5px;
+        border-top: 1px solid #eaeaea;
+        color: #777;
+        font-size: 13px;
+        font-weight: bold;
+
+    }
+    footer a {
+        color: #555;
+    }
+    footer a:hover {
+        color: #222;
+    }
+    footer small {
+        float: left;
+    }
+    footer img.brand-icon {
+        width: 17px;
+        height: 17px;
+    }
+</style>
+<footer class="footer">
+    <img class="brand-icon" src="https://cdn.learnku.com//uploads/communities/WtC3cPLHzMbKRSZnagU9.png!/both/44x44">
+ <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about" >关于</a>
+    </div>
+</footer>
